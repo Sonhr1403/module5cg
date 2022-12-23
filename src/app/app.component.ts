764ddff868dc92ac142b1dbf7d5adced9b3b64ab
Sonhr1403 <sonhr1403@gmail.com>
2022-12-23 16:33:48 +0700

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-change-font-size';
+
+  fontSize = 14;
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
